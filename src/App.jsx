b3285b0e15eec5887db2a4f0d61b8c817e848afc
kartik mahato <118644047+kartikcod@users.noreply.github.com>
2025-07-13 13:51:25 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+    <Banner/>
+    <Footer />
+    
+   
+    </>
+  )
+}
+
+export default App;
